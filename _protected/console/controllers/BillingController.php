63547a13f6b1be67093681e9540ed41084eb9f14
virgantara <oddy@unida.gontor.ac.id>
2021-06-17 21:07:18 +0700

    fix update

--- _protected/console/controllers/BillingController.php
+++ _protected/console/controllers/BillingController.php
@@ -3,6 +3,7 @@
 namespace app\console\controllers;
 
 use Yii;
+use app\models\Tahun;
 use app\models\Tagihan;
 use app\models\SimakDatakrs;
 use app\models\SimakMastermahasiswa;
@@ -17,6 +18,131 @@ use yii\web\NotFoundHttpException;
  */
 class BillingController extends Controller
 {
+
+    public function actionAktivasi()
+    {
+
+        $tahun = Tahun::getTahunAktif();
+        
+
+
+        $sa = $_POST['status_aktivitas'];
+        $query = SimakMastermahasiswa::find()->where([
+            'tahun_masuk' => 2021,
+            // 'status_aktivitas' => 'N'
+        ]);
+
+
+        $listCustomer = $query->all();
+        $transaction = \Yii::$app->db->beginTransaction();
+        $errors = '';
+        
+        try 
+        {
+
+            
+
+            $counter = 0;
+            foreach($listCustomer as $c)
+            {
+
+                $query = Tagihan::find();
+                $query->alias('t');
+                $query->joinWith(['komponen as k','komponen.kategori as kk']);
+                $query->andWhere([
+                    'kk.kode' => '01',
+                    't.nim' => $c->nim_mhs,
+                    't.tahun' => $tahun->id,
+                ]);
+
+                // $query->andWhere('terbayar >= nilai_minimal');
+                $lunas = $query->one();
+                
+                if(!empty($lunas))
+                {
+                    $lunas->nilai_minimal = 10000;
+                    $lunas->terbayar = 10000;
+                    if(!$lunas->save(false,['nilai_minimal','terbayar']))
+                    {
+                        $errors .= \app\helpers\MyHelper::logError($lunas);
+                        throw new \Exception;
+                    }
+
+                    $c->status_aktivitas = 'A';
+                    if($c->save(false,['status_aktivitas']))
+                    {
+                        $konfirm = SimakKonfirmasipembayaran::find()->where([
+                            'nim' =>$c->nim_mhs,
+                            'pembayaran' => '01',
+                            'semester' => $c->semester,
+                            'jumlah' => $lunas->terbayar,
+                            'bank' => 'nama_bank',
+                            'tahun_id' => $tahun->id
+                        ])->one();
+
+                        if(empty($konfirm))
+                        {
+                            $konfirm = new SimakKonfirmasipembayaran;
+                            $konfirm->nim = $c->nim_mhs;
+                            $konfirm->pembayaran = '01';
+                            $konfirm->semester = $c->semester;
+                            $konfirm->jumlah = $lunas->terbayar;
+                            $konfirm->bank = 'nama_bank';
+                            $konfirm->tahun_id = $tahun->id;
+                            $konfirm->status = 1;
+                        }
+
+                        else{
+                            $konfirm->pembayaran = '01';
+                            $konfirm->jumlah = $lunas->terbayar;
+                            $konfirm->status = 1;
+                        }
+
+                        if($konfirm->save())
+                        {
+                            $counter++;
+                        }
+
+                        else{
+                            $errors .= \app\helpers\MyHelper::logError($konfirm);
+                            throw new \Exception;
+                        }
+                        
+                    }    
+
+                    else{
+                        $errors .= \app\helpers\MyHelper::logError($c);
+                        throw new \Exception;
+                    }
+                }
+
+
+
+            }
+            $transaction->commit();
+            echo $counter." Data mahasiswa telah diaktifkan";
+
+            
+        } catch (\Exception $e) {
+            $errors .= $e->getMessage();
+            $transaction->rollBack();
+            echo $errors;
+            
+            
+        } catch (\Throwable $e) {
+            $errors .= $e->getMessage();
+            $transaction->rollBack();
+            echo $errors;
+            
+            
+            
+        }
+        
+        echo "\n";
+        return ExitCode::OK;
+        
+    }
+
 
     public function actionUpdateStatus($tahun)
     {
